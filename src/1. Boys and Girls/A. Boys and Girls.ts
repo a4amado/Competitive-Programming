24bb1526a7c2cdc038748d5603b2d70e7a888019
Ahmad Addel <a4addel@gmail.com>
2023-09-06 01:22:42 +0200

    submit 2. Vanya and Fence
    solve 2. Vanya and Fence
    link: https://codeforces.com/contest/677/problem/A

---
+++ src/1. Boys and Girls/A. Boys and Girls.ts
@@ -0,0 +1,73 @@
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+const input = readFileSync(join(__dirname, "input.txt")).toString("ascii");
+const nums: string[] = input.split(" ")
+
+
+function main() {
+    let boys = parseInt(nums[0] as string, 10);
+    let girls = parseInt(nums[1] as string, 10);
+
+
+    const letters: Array<"B" | "G"> = [];
+
+
+    const g = rec(letters, boys, girls).join("")
+    console.log(g);
+    writeFileSync(join(__dirname, "output.txt"), g)
+
+
+
+}
+main()
+
+function rec(l: Array<"B" | "G" | null>, b: number, g: number) {
+
+    if (shouldIInsertAB(l, b, g)) {
+        l.push("B")
+        b--;
+        return rec(l, b, g)
+    } else if (shouldIInsertAG(l, b, g)) {
+        l.push("G")
+        g--;
+        return rec(l, b, g)
+    }
+    return l;
+
+}
+
+function shouldIInsertAB(l: Array<"B" | "G" | null>, b: number, g: number) {
+    if (b === 0) return false;
+
+    if (l.length === 0) {
+        if (g < b || g == b) return true
+        else return false
+    }
+
+    if (l[l.length - 1] === "B") {
+        if (g === 0) return true;
+        else return false;
+    }
+
+
+    return true;
+
+
+}
+
+function shouldIInsertAG(l: Array<"B" | "G" | null>, b: number, g: number) {
+    if (g === 0) return false;
+    if (l.length === 0) {
+        if (g > b || g == b) return true
+        else return false
+    }
+    if (l[l.length - 1] === "G") {
+        if (b === 0) return true;
+        else return false;
+    }
+
+
+    return true;
+
+
+}
---
+++ src/2. Vanya and Fence/Vanya and Fence.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const process_1 = require("process");
+const readline_1 = require("readline");
+const rl = (0, readline_1.createInterface)({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+let numberOfFriends;
+let heightOfTheFence;
+let heights = [];
+rl.on("line", (input) => {
+    if (!numberOfFriends || !heightOfTheFence) {
+        const [n, h] = input.trim().split(" ").map(e => parseInt(e));
+        numberOfFriends = n;
+        heightOfTheFence = h;
+    }
+    else if (heights.length === 0) {
+        heights = input.trim().split(" ").map(e => parseInt(e, 10)).filter(e => Number.isInteger(e));
+        rl.close();
+    }
+});
+rl.once("close", () => {
+    let road_width = 0;
+    for (let index = 0; index < heights.length; index++) {
+        // @ts-ignore
+        if (heights[index] > heightOfTheFence) {
+            road_width += 2;
+        }
+        else {
+            road_width++;
+        }
+    }
+    process_1.stdout.write(road_width.toString(), () => {
+        process.exit(0);
+    });
+});
