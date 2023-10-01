185b282afcb8aa46ceb0e0c9552063bcd161d63b
Ahmad Addel <a4addel@gmail.com>
2023-10-01 08:12:41 +0300

    add new folder

---
+++ Level A/16. Night at the Museum/Night at the Museum.cpp
@@ -0,0 +1,15 @@
+/**
+ * Name: Night at the Museum
+ * link: https://codeforces.com/contest/731/problem/A
+*/
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+int main() {
+
+
+
+    return 0;
+}
