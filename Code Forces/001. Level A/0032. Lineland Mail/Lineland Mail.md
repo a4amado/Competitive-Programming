12c5d3972e5732bcbfe2e50c60669fc57c23e50a
Ahmad Addel <a4addel@gmail.com>
2023-10-05 14:06:19 +0300

    solve `0032. Lineland Mail`

---
+++ Code Forces/001. Level A/0032. Lineland Mail/Lineland Mail.md
@@ -0,0 +1,9 @@
+both over 0
+    bigger - smaller
+bother under 0
+    abs(smaller) - abs(bigger)
+
+one is under one is bigger
+    one over zero - abs(one under zero)
+
+
---
+++ Code Forces/001. Level A/0032. Lineland Mail/Lineland Mail.py
@@ -0,0 +1,27 @@
+number_of_cities = input()
+
+l_of_cities = [None for _ in range(int(number_of_cities))]
+
+cities = input().split(" ")
+
+for idx, element in enumerate(cities):
+    l_of_cities[int(idx)] = int(element)
+
+l_of_cities.sort()
+
+
+for idx, element in enumerate(l_of_cities):
+    # if first city
+    if idx == 0:
+        print(abs(l_of_cities[idx + 1]  - element) ,  abs(element - (l_of_cities[len(l_of_cities)  -1])))
+        continue
+    # if last city
+    if idx == len(l_of_cities) - 1:
+        print(abs(element - l_of_cities[idx - 1]),  abs(element  - (l_of_cities[0])))
+        continue
+    else:
+        min_i = min(abs(element - l_of_cities[idx - 1]), abs(l_of_cities[idx + 1] - element))
+        max_i = max(abs(element - l_of_cities[0]), abs(l_of_cities[len(l_of_cities)  -1] - element))
+
+        print(min_i, max_i)
+        
