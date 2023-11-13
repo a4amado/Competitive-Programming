172e800a9773237c1686b5e6c28757db68aed444
Ahmad Addel <a4addel@gmail.com>
2023-11-13 03:45:05 +0200

    solve `0047. Arpa hard exam and Mehrdad’s naive cheat [A742]`

--- Code Forces/001. Level A/0047. Arpa hard exam and Mehrdad’s naive cheat [A742]/Arpa hard exam and Mehrdad’s naive cheat.cpp
+++ Code Forces/001. Level A/0047. Arpa hard exam and Mehrdad’s naive cheat [A742]/Arpa hard exam and Mehrdad’s naive cheat.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+typedef long long int ll;
 
 using namespace std;
+int main()
 
-int main() {
+{
+
+    ll n;
+    cin >> n;
+
+    if (n == 0)
+    {
+        cout << "1";
+        return 0;
+    }
 
 
+    if (n % 4 == 0)
+    {
+        cout << "6" << endl;
+    }
+    else if (n % 4 == 1)
+    {
+        cout << "8" << endl;
+    }
+    else if (n % 4 == 2)
+    {
+        cout << "4" << endl;
+    }
+    else if (n % 4 == 3)
+    {
+        cout << "2" << endl;
+    }
 
     return 0;
 }
---
+++ Online Judge/001. Level A/0003. 408 - Uniform Generator/Uniform Generator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    // seed(x + 1) = [seed(x) + STEP]%MOD
+
+
+    return 0;
+}
