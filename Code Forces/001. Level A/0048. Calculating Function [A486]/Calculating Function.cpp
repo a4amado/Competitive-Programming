abdacfc5b595770f05501ff382d6c53a7cf9d6d1
Ahmad Addel <a4addel@gmail.com>
2023-11-15 01:38:28 +0200

    solve `Calculating Function`

--- Code Forces/001. Level A/0048. Calculating Function [A486]/Calculating Function.cpp
+++ Code Forces/001. Level A/0048. Calculating Function [A486]/Calculating Function.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <math.h>
+#include <cstdint>
+#include <iomanip>
 
 using namespace std;
 
+typedef long double ll;
 
+int main()
+{
 
-int main() {
+    int64_t n;
+    cin >> n;
 
+    if (n % 2 == 0)
+    {
 
+        cout << std::fixed << std::setprecision(0) << (ll)abs(ceil((ll)n / 2)) << endl;
+        return 0;
+    }
 
-
-
-
+    int64_t a = -ceil((ll)n / 2);
+    cout << std::fixed << std::setprecision(0) << a << endl;
     return 0;
 }
