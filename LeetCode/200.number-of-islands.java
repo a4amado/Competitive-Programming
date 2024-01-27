85e349d29ea299a7f2e83dbcc0de9a8fbd2cf9d6
Ahmad Addel <a4addel@gmail.com>
2024-01-27 09:53:53 +0200

    Update 200.number-of-islands.java

--- LeetCode/200.number-of-islands.java
+++ LeetCode/200.number-of-islands.java
@@ -4,50 +4,48 @@
  * [200] Number of Islands
  */
 
-
 import java.util.*;
+
 // @lc code=start
+
 class Solution {
     public int numIslands(char[][] grid) {
-        Map<String, Boolean> visted = new HashMap<>();
-        int[] n = new int[1];
-        n[0] = 0;
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0; // handle null or empty grid
+        }
 
-        for (int row  = 0; row < grid.length; row++) {
-            for (int col  = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == '1' && !visted.containsKey(row + "" + col)) {
-                    n[0]++;
-                    this.check_all_conected_land(grid, visted, row, col);
-                } else {
-                    visted.put(row +"" + col, true);
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        int[] islandCount = new int[1];
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+
+                if (grid[row][col] == '1' && visited[row][col] != true) {
+                    this.checkAllConnectedLand(grid, visited, row, col);
+                    islandCount[0]++;
                 }
 
             }
         }
-        return n[0];
+        return islandCount[0];
     }
 
-    private void check_all_conected_land(char[][] grid, Map<String, Boolean> visted, int  row, int col) {
-        if (this.isOutOfBound(grid, visted, row, col)) return;
+    private void checkAllConnectedLand(char[][] grid, boolean[][] visited, int row, int col) {
+        if (isOutOfBound(grid, row, col) || visited[row][col] || grid[row][col] == '0')
+            return;
 
-        if (visted.containsKey(row +"" + col)) return;
-        if (grid[row][col] == '0') return;
-        
-        visted.put(row +"" + col, true);
+        visited[row][col] = true;
 
-        this.check_all_conected_land(grid, visted, row - 1, col);
-        this.check_all_conected_land(grid, visted, row + 1, col);
-        this.check_all_conected_land(grid, visted, row, col - 1);
-        this.check_all_conected_land(grid, visted, row, col + 1);
+        checkAllConnectedLand(grid, visited, row - 1, col);
+        checkAllConnectedLand(grid, visited, row + 1, col);
+        checkAllConnectedLand(grid, visited, row, col - 1);
+        checkAllConnectedLand(grid, visited, row, col + 1);
     }
 
-    private boolean isOutOfBound(char[][] grid, Map<String, Boolean> visited, int row, int col) {
-        if (row < 0 || row >= grid.length) return true;
-        if (col < 0 || col >= grid[0].length) return true;
-        return false;
+    private boolean isOutOfBound(char[][] grid, int row, int col) {
+        return row < 0 || row >= grid.length || col < 0 || col >= grid[0].length;
     }
-    
 }
+
 // @lc code=end
-
-
