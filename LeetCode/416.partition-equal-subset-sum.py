70f837ec7c2acc5bcb14e23acc67c92455f42b67
Ahmad Addel <a4addel@gmail.com>
2024-09-18 23:16:52 +0300

    solve some more

---
+++ LeetCode/416.partition-equal-subset-sum.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
+        
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        
+        target = total_sum // 2
+        memo = {}
+        
+        def backtracking(i: int, currSum: int):
+            if currSum == target:
+                return True
+            
+            if (i, currSum) in memo:
+                return False
+            if currSum > target:
+                return False
+            if i >= len(nums):
+                return False
+
+            
+            if backtracking(i+1, currSum + nums[i]):
+                return True
+            if backtracking(i+1, currSum):
+                return True
+            memo[(i, currSum)] = False
+
+            return False
+
+        return backtracking(0, 0)
+
+
+s = Solution()
+s.canPartition([1,5,11,5])
