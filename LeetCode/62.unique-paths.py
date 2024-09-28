28de7e666565ef7cfaac4082c8e89fed9026409c
Ahmad adel <a4addel@gmail.com>
2024-09-28 13:12:56 +0300

    solve some more DP

--- LeetCode/62.unique-paths.py
+++ LeetCode/62.unique-paths.py
@@ -8,16 +8,44 @@ from typing import List
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1 for x in range(n)] for _ in range(m)]
+        def dfs(row:int, col: int, path: List[(int)], visited: List[List[int]]):
+            if self.isOutOfRange(row, col, len(visited), len(visited[0])):
+                return False
 
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            if visited[row][col]:return False
+            
+            if row == len(visited) -1 and col == len(visited[0]) - 1: 
+                
+                return 1
+            
+            visited[row][col]= True
+            path.append((row, col))
+            l = dfs(row+1, col, path, visited)
+            r= dfs(row, col +1, path, visited)
+
+            path.pop()
+            visited[row][col] = False
+            return l + r
         
-        return dp[m - 1][n - 1]
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        
+        return dfs(0,0, [], visited)
 
+        # dp = [[1 for x in range(n)] for _ in range(m)]
+
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        # return dp[m - 1][n - 1]
+    def isOutOfRange(seld, row:int,  col:int, maxRow:int, maxCol: int):
+        if row < 0:return True
+        if col < 0:return True
+        if row >= maxRow:return True
+        if col >= maxCol:return True
+        return False
 
 s = Solution()
-s.uniquePaths(3, 7)
+print(s.uniquePaths(3, 7))
 # @lc code=end
 
---
+++ code360/Unique Paths II.py
@@ -0,0 +1,27 @@
+from typing import *
+MOD = 10**9 + 7
+
+def mazeObstacles(n: int, m:int, mat: List[List[int]]):
+    rows, cols = len(mat), len(mat[0])
+    dp = [
+        [0 for x in range(cols)] for _ in range(rows)
+    ]
+
+    for cdx, col in enumerate(mat[0]):
+        if col == -1:break
+        dp[0][cdx] = 1
+
+    for rdx in range(len(mat)):
+        if mat[rdx][0] == -1:break
+        dp[rdx][0] = 1
+
+    for rdx in range(1, rows):
+        for cdx in range(1, cols):
+            if mat[rdx][cdx] == -1:
+                continue
+            else:
+                dp[rdx][cdx] = (dp[rdx - 1][cdx] + dp[rdx][cdx - 1]) % MOD
+
+
+    return dp[-1][-1]
+
