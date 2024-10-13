c815458150d3e9ea4a3c5b0e80775558b2b984ac
Ahmad Addel <a4amado@gmail.com>
2024-10-14 01:58:28 +0300

    Time: 367 ms (72.65%) | Memory: 21.1 MB (45.63%) - LeetSync

--- LeetCode/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ LeetCode/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,9 +1,10 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         memo = {}
-        def dp(i: int, j: int) -> int:
+        def d(i: int, j: int) -> int:
             key = (i, j)
             if key in memo: return memo[key]
+
             # Base cases
             if i == len(s1):
                 memo[key] =  sum(ord(c) for c in s2[j:])
@@ -15,12 +16,39 @@ class Solution:
             
             # If characters match, no deletion needed
             if s1[i] == s2[j]:
-                return dp(i + 1, j + 1)
+                return d(i + 1, j + 1)
             
             # Otherwise, try deleting from either string and choose the minimum
             memo[key] = min(
-                ord(s1[i]) + dp(i + 1, j),
-                ord(s2[j]) + dp(i, j + 1)
+                ord(s1[i]) + d(i + 1, j),
+                ord(s2[j]) + d(i, j + 1)
             )
             return memo[key]
-        return dp(0, 0)
+        
+        # return dp(0, 0)
+        rows = len(s1) + 1
+        cols = len(s2)  + 1
+
+        dp = [
+            ([0] * cols) for _ in range(rows)
+        ]
+
+        for j in range(1, rows):
+            dp[j][0] = dp[j-1][0] + ord(s1[j-1])
+        for i in range(1, cols):
+            dp[0][i] = dp[0][i-1] + ord(s2[i-1])
+        
+
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                if s1[row-1] == s2[col-1]:
+                    dp[row][col] = dp[row-1][col-1]
+                else:
+                    dp[row][col] = min(
+                                    ord(s2[col-1]) + dp[row][col-1],
+                                    ord(s1[row-1]) + dp[row-1][col]
+                                )
+                    
+                
+        return dp[-1][-1]
