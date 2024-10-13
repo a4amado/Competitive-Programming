53d2bf35d2c89a1248ee20090681a5eef00c5c05
Ahmad Addel <a4amado@gmail.com>
2024-10-13 22:25:59 +0300

    Time: 405 ms (55.14%) | Memory: 124.2 MB (18.68%) - LeetSync

--- LeetCode/44-wildcard-matching/wildcard-matching.py
+++ LeetCode/44-wildcard-matching/wildcard-matching.py
@@ -5,6 +5,7 @@ class Solution:
         wildcard = "*"
         singular = "?"
         memo = {}
+
         def d(idx:int, jdx: int):
             if idx == len(pattern) and jdx == len(text):
                 return True
@@ -24,13 +25,9 @@ class Solution:
                 memo[key] = d(idx+1, jdx+1)
             elif wildcard == currpattern:
                 memo[key] = d(idx+1, jdx) or d(idx, jdx+1)
-            elif currpattern != currChar:
-                memo[key] = False
             else:
-                memo[key] = d(idx + 1, jdx + 1)
+                memo[key] = currpattern == currChar and d(idx + 1, jdx + 1)
             
             
             return memo[key]
         return d(0,0)
-            
-
