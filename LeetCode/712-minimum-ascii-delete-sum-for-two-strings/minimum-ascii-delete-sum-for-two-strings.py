81d91b36fc19069319e844f9d0377c2361704dd7
Ahmad Addel <a4amado@gmail.com>
2024-10-14 01:11:09 +0300

    Time: 839 ms (19.85%) | Memory: 202.6 MB (22.42%) - LeetSync

--- LeetCode/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ LeetCode/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -2,13 +2,17 @@ class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         memo = {}
         def dp(i: int, j: int) -> int:
-            # Base cases
-            if i == len(s1):
-                return sum(ord(c) for c in s2[j:])
-            if j == len(s2):
-                return sum(ord(c) for c in s1[i:])
             key = (i, j)
             if key in memo: return memo[key]
+            # Base cases
+            if i == len(s1):
+                memo[key] =  sum(ord(c) for c in s2[j:])
+                return memo[key]
+            if j == len(s2):
+                memo[key] =  sum(ord(c) for c in s1[i:])
+                return memo[key]
+            
+            
             # If characters match, no deletion needed
             if s1[i] == s2[j]:
                 return dp(i + 1, j + 1)
