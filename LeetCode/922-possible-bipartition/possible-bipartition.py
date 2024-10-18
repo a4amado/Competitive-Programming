5fcd4a233b314a8ce6cd16bef1c9584a43866b5c
Ahmad Addel <a4amado@gmail.com>
2024-10-18 09:44:38 +0300

    Time: 71 ms (100.00%) | Memory: 29.7 MB (5.39%) - LeetSync

---
+++ LeetCode/922-possible-bipartition/possible-bipartition.py
@@ -0,0 +1,23 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        adjs = {idx: set() for idx in range(1, n+1)}
+        for u,v in dislikes:
+            adjs[u].add(v)
+            adjs[v].add(u)
+
+        groups = {idx: {} for idx in range(1, n+1)}
+
+        def d(idx: int, group: int):
+            if groups[idx]:
+                return groups[idx] == group
+
+            groups[idx] = group
+            
+            for i in adjs[idx]:
+                if not d(i, 1 if group == 2 else 2):
+                    return  False
+            return True
+        for i in range(1, n + 1):
+            if not groups[i] and not d(i, 1):
+                return False
+        return True
