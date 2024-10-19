29a6cc839f7a51b20b3cc6070fa100847c862dae
Ahmad Addel <a4amado@gmail.com>
2024-10-19 09:35:17 +0300

    Time: 46 ms (100.00%) | Memory: 17 MB (77.26%) - LeetSync

--- LeetCode/1129-longest-string-chain/longest-string-chain.py
+++ LeetCode/1129-longest-string-chain/longest-string-chain.py
@@ -6,14 +6,9 @@ class Solution:
         words.sort(key=len)
         
         # Dictionary to store the longest chain ending at each word
-        dp = {}
-        
-        max_chain = 0
-        
+        dp = {word:1 for word in words}
+
         for word in words:
-            # Initialize the chain length for the current word
-            dp[word] = 1
-            
             # Try removing one character at a time
             for i in range(len(word)):
                 prev = word[:i] + word[i+1:]
@@ -22,7 +17,5 @@ class Solution:
                 if prev in dp:
                     dp[word] = max(dp[word], dp[prev] + 1)
             
-            # Update the maximum chain length
-            max_chain = max(max_chain, dp[word])
         
-        return max_chain
+        return max(dp.values())
