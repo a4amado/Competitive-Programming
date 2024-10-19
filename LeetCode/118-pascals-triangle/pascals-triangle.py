d0671722c2064a49a0e87be6db6ac6531c0a8296
Ahmad Addel <a4amado@gmail.com>
2024-10-19 15:02:05 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (9.78%) - LeetSync

---
+++ LeetCode/118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,29 @@
+from typing import *
+# @lc code=start
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        def parent(row: int, col: int):
+            newRow = row -1
+            left = col -1
+            right = col 
+            res = [newRow]
+            if col == 0:
+                res.append(0)
+            elif col == row:
+                res.append(col-1)
+            else:
+                res.append(left)
+                res.append(right)
+            return res
+
+        pascal = [
+            [1] * idx for idx in range(1, numRows + 1)
+        ]
+        for row in range(numRows):
+            for col in range(row):
+                parents = parent(row, col)
+                if len(parents) < 3:
+                    pascal[row][col] = 1
+                else:
+                    pascal[row][col] = pascal[parents[0]][parents[1]]  + pascal[parents[0]][parents[2]]
+        return pascal
