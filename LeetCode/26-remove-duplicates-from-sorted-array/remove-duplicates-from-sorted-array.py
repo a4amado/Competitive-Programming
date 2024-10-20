8022ef0116d8e5c198d7192bf0d16f3703f87992
Ahmad Addel <a4amado@gmail.com>
2024-10-20 06:04:31 +0300

    Time: 11 ms (98.90%) | Memory: 13.2 MB (51.50%) - LeetSync

--- LeetCode/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ LeetCode/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,17 +1,22 @@
+# @lc code=start
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        uniqie = set()
-        for idx, item in enumerate(nums):
-            uniqie.add(item)
-        nums_ = [x for x in uniqie]
-        for i in range(len(nums) - len(uniqie)):
-            nums_.append("_")
-        nums_.sort()
-        for i in range(len(nums_)):
-            nums[i] = nums_[i]
-        return len(uniqie)
+        
+        
+        done = set()
+        newArray = []
+        for i in nums:
+            if i not in done:
+                newArray.append(i)
+                done.add(i)
+        
+        for i in range(len(nums)):
+            if i < len(newArray):
+                nums[i] = newArray[i]
+            else:
+                nums[i] = '_'
+        return len(newArray)
