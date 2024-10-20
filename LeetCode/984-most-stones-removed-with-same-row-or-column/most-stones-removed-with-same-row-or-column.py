61c2133e371268f9a796b747d0b303c41372473e
Ahmad Addel <a4amado@gmail.com>
2024-10-20 03:04:21 +0300

    Time: 70 ms (99.88%) | Memory: 20.3 MB (6.10%) - LeetSync

--- LeetCode/984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ LeetCode/984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,19 +1,55 @@
 from typing import List
 from collections import defaultdict
 
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = defaultdict(int)
+
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
+            return
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] += 1
+
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        def dfs(i, j):
-            visited.add((i, j))
-            for ni, nj in stones:
-                if (ni, nj) not in visited and (ni == i or nj == j):
-                    dfs(ni, nj)
+        uf = UnionFind()
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        
+        for i, (r, c) in enumerate(stones):
+            rows[r].append(i)
+            cols[c].append(i)
+
+        def dfs(i):
+            if i in visited:
+                return
+            visited.add(i)
+            r, c = stones[i]
+            for j in rows[r]:
+                uf.union(i, j)
+                dfs(j)
+            for j in cols[c]:
+                uf.union(i, j)
+                dfs(j)
 
         visited = set()
-        islands = 0
-        for i, j in stones:
-            if (i, j) not in visited:
-                dfs(i, j)
-                islands += 1
+        for i in range(len(stones)):
+            dfs(i)
 
-        return len(stones) - islands
+        roots = set(uf.find(i) for i in range(len(stones)))
+        return len(stones) - len(roots)
