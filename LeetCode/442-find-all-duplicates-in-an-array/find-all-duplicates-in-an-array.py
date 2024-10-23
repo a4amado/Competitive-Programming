8755e0b42ae8cb583737304e42e84f849c472602
Ahmad Addel <a4amado@gmail.com>
2024-10-23 10:39:32 +0300

    Time: 27 ms (97.50%) | Memory: 23.1 MB (97.27%) - LeetSync

--- LeetCode/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ LeetCode/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,17 +1,18 @@
+#
+from typing import *
+
+# @lc code=start
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        result = []
-            
-        # Iterate through each number in the array
+        res = []
+
         for num in nums:
-            # Get the absolute value since the number might be negative due to previous marking
-            index = abs(num) - 1
+            idx = abs(num) - 1
             
-            # If the number at index is negative, we've seen this number before
-            if nums[index] < 0:
-                result.append(abs(num))
-            # Otherwise, mark it as seen by making it negative
+            if nums[idx] < 0:
+                res.append(abs(num))
             else:
-                nums[index] = -nums[index]
-        
-        return result
+                nums[idx] = -nums[idx]
+            
+
+        return res
